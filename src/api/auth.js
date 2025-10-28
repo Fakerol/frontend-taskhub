31e480ab0d2cfe1bbf0fa50f8683802061647468
Fakhrul <96655716+Fakerol@users.noreply.github.com>
2025-10-28 23:19:03 +0800

    baseURL update for docker

--- src/api/auth.js
+++ src/api/auth.js
@@ -5,7 +5,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // Create axios instance
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: 'http://54.234.133.14:5000/api/',
   headers: {
     'Content-Type': 'application/json',
   },
